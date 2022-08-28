1bffd9bade1d8b31a95c1a137613cbc23b27bd53
Eric Lee <szl0094@auburn.edu>
2022-08-28 17:08:40 -0500

    init

---
+++ Assets/Scripts/AstroSlug.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroSlug : MonoBehaviour {
+    private bool _isIdle = true;
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
