dff43dc08c3b605105b6346debd58228aab2a5ff
Eric Lee <szl0094@auburn.edu>
2022-08-28 20:22:51 -0500

    wave path and wave config scriptable objects scripts added

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject {
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] private float _moveSpeed = 5f;
+
+    public Transform GetStartingWaypoint() {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints() {
+        List<Transform> waypoints = new List<Transform>();
+
+        foreach(Transform waypoint in pathPrefab) {
+            waypoints.Add(waypoint);
+        }
+
+        return waypoints;
+    }
+
+    public float GetMoveSpeed() {
+        return _moveSpeed;
+    }
+}
