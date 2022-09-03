41fc18d522efc023fc344ae35eb0e8cfeea16d5d
Eric Lee <szl0094@auburn.edu>
2022-09-03 12:31:15 -0500

    Format

--- Assets/Scripts/SpriteNumber.cs
+++ Assets/Scripts/SpriteNumber.cs
@@ -4,22 +4,20 @@ using System.Collections;
 public class SpriteNumber : MonoBehaviour {
 
 
-	
-	public Sprite[] numbers;
-	[UnityEngine.HideInInspector]
-	public SpriteRenderer render;
 
-	// Use this for initialization
-	void Awake ()
-	{
-		//used so that we can use the Bounds function
-		render = this.GetComponent<SpriteRenderer> ();
-		render.sprite = numbers [0]; //temporarily set the number to 0
-	}
+    public Sprite[] numbers;
+    [UnityEngine.HideInInspector]
+    public SpriteRenderer render;
+
+    // Use this for initialization
+    void Awake() {
+        //used so that we can use the Bounds function
+        render = this.GetComponent<SpriteRenderer>();
+        render.sprite = numbers[0]; //temporarily set the number to 0
+    }
 
 
-	public void SetNumber( int number)
-	{
-		this.gameObject.GetComponent<SpriteRenderer> ().sprite = numbers [number];
-	}
+    public void SetNumber(int number) {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = numbers[number];
+    }
 }
